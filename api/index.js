41593468beab6a37e0b18abbe95bc872071ff7a6
João Paulo <jotapeemaria@gmail.com>
2025-11-22 16:09:28 -0400

    add models connection

--- api/index.js
+++ api/index.js
@@ -9,14 +9,13 @@ const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(morgan("dev"));
 
-app.use("barber-shop/", require("./src/routes/barber-shop.routes"));
+app.use("/barber-shop", barberShop);
 
 // VARIABLES
 app.set("port", 8000);
-("");
 app.listen(app.get("port"), () => {
   console.log(`Listening on port ${app.get("port")}`);
 });
